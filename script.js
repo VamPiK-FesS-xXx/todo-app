e83ac3e59a13140572781f46d1398e61edf55369
/vammpik <vampik077@gmail.com>
2026-07-21 21:50:49 +0300

    finnaly slides are works

--- script.js
+++ script.js
@@ -5,25 +5,7 @@ const taskList = document.querySelector('.tasks__list');
 //styled variables
 const label = document.querySelector('label');
 const inputContainer = document.querySelector('.todo__container-functional');
-//object for notes will init with this obj
 
-//comments
-
-/**
- *
- * fix bug with input display error
- * think about objects
- * think about template render
- * later
- * ---
- * also think about controls on tasks
- * ---
- * make a slide effect or drag and drop
- * make a notification and animation that user can slide
- * his notes
- * ---
- * save all notes in localstroge and render they
- */
 const notes = [
 	{
 		task: '123',
@@ -35,86 +17,61 @@ const notes = [
 	},
 ];
 
-/**
- * think about make one more object in object
- * make variable newNote into notes to try to save data
- * from input value and then push it into template
- *
- */
 document.addEventListener('DOMContentLoaded', initializeNotes);
 
-function initializeNotes() {
+function initializeNotes(event) {
+	let offsetX = 0;
 	const userTextInNote = taskList.querySelectorAll('.tasks__list-text');
 	userTextInNote.forEach((note) => {
-		// note.addEventListener('pointerdown', (event) => {
-		// 	const positiveLayerX = event.layerX; // add a minus in begin if need a negative transform
-		// 	console.log(positiveLayerX);
-		// 	note.style.transform = `translateX(${positiveLayerX}px`;
-		// });
-		// note.addEventListener('pointermove', (e) => {
-		// 	console.log(e.target);
-		// 	if (e.target) {
-		// 		note.addEventListener('pointerdown', () => {
-		// 			const positiveLayerX = e.layerX; // add a minus in begin if need a negative transform
-		// 			console.log(positiveLayerX);
-		// 			note.style.transform = `translateX(${positiveLayerX}px`;
-		// 		});
-		// 	}
-		// });
+		function beginSliding(e) {
+			offsetX = e.clientX - note.getBoundingClientRect().left;
+			console.log(offsetX);
+			note.onpointermove = slide;
+			note.setPointerCapture(e.pointerId);
+			console.log(e);
+		}
 
-		note.addEventListener('pointerup', (e) => {
-			// console.log(e.target);
-			e.preventDefault();
-			if (e.target) {
-				note.addEventListener('pointermove', (e) => {
-					e.preventDefault();
+		function stopSliding(e) {
+			note.onpointermove = null;
+			note.releasePointerCapture(e.pointerId);
+		}
+		function slide(e) {
+			const clientX = e.clientX - offsetX;
+			note.style.transform = `translateX(${clientX}px`;
+			console.log(e.clientX);
+		}
 
-					const positiveLayerX = e.layerX; // add a minus in begin if need a negative transform
-					note.style.transform = `translateX(${parseInt(positiveLayerX / 25)}%`;
-					console.log(parseInt(positiveLayerX / 5));
-				});
-			}
-		});
+		note.onpointerdown = beginSliding;
+		note.onpointerup = stopSliding;
 	});
-
-	function slideNote(note) {}
-	// userTextInNote.forEach((note, index) => {
-	// 	note.addEventListener('poi', (e) => {
-	// 		const positiveClientX = e.clientX;
-	// 		const negativeClientX = -e.clientX;
-	// 		userTextInNote.style.transform = `translateX(${negativeClientX}px`;
-	// 		if (negativeClientX >= -25) {
-	// 			console.log('delete note');
-	// 		}
-	// 	});
-	// });
-	/**
-	 *FOR NEGATIVE X 100 PX FOR POSITIVE 120 PX
-	 * i recive all userNotes
-	 * a need to make on all notes addEventListener
-	 * for drag it on left or right
-	 * ---
-	 * also i need to think about drag 'item'
-	 * now i can drag only text and see the changes
-	 * but i need to drag the note body and slide it
-	 * now i need to think about it
-	 * ---
-	 * UPD
-	 * i can recieve a clientX and paste it in the style for transform it
-	 * also i need to do it smooth for user
-	 * i need to think how now i can transform my tasks by X
-	 * ---
-	 * OKAY I MADE IT SLIDE
-	 * but the first i need to know more about how drag work in web
-	 * also i need to make -x and x for complete and delete
-	 * my error was in one single s that i placed in a wrong place
-	 * okay i need to receive negative and positive x's and then i can change my classes or delete notes
-	 * ---
-	 * need to make my notes slide only on x axis and drag it only on the left or right
-	 * now i have a problem with one that, that i'm using a drag and it take item and wait
-	 * till i'm drop it into the drop zone
-	 */
 }
+
+/**
+ *FOR NEGATIVE X 100 PX FOR POSITIVE 120 PX
+ * i recive all userNotes
+ * a need to make on all notes addEventListener
+ * for drag it on left or right
+ * ---
+ * also i need to think about drag 'item'
+ * now i can drag only text and see the changes
+ * but i need to drag the note body and slide it
+ * now i need to think about it
+ * ---
+ * UPD
+ * i can recieve a clientX and paste it in the style for transform it
+ * also i need to do it smooth for user
+ * i need to think how now i can transform my tasks by X
+ * ---
+ * OKAY I MADE IT SLIDE
+ * but the first i need to know more about how drag work in web
+ * also i need to make -x and x for complete and delete
+ * my error was in one single s that i placed in a wrong place
+ * okay i need to receive negative and positive x's and then i can change my classes or delete notes
+ * ---
+ * need to make my notes slide only on x axis and drag it only on the left or right
+ * now i have a problem with one that, that i'm using a drag and it take item and wait
+ * till i'm drop it into the drop zone
+ */
 input.addEventListener('keydown', () => {
 	//fix the bag with empty input later
 	if (input.value.trim() === '') {
@@ -182,22 +139,11 @@ function noteTemplate(note) {
 	);
 }
 //render fucntion which sort through notes object
-/**
- * need to make bg green and red with text in bgs
- * find x in client and if x = 60 task change classname
- * else if x = ??? task delete from array
- * bg maybe can make with before after
- */
+
 function renderNote() {
 	notes.forEach((note) => {
 		noteTemplate(note);
-		initializeNotes(note);
 	});
 }
 
 renderNote();
-
-/**
- *
- * new comments
- */
