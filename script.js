c557f3ff4769907028e110c243683e8dfc320de6
/vammpik <vampik077@gmail.com>
2026-07-21 22:01:34 +0300

    on main

--- script.js
+++ script.js
@@ -7,23 +7,6 @@ const label = document.querySelector('label');
 const inputContainer = document.querySelector('.todo__container-functional');
 //object for notes will init with this obj
 
-//comments
-
-/**
- *
- * fix bug with input display error
- * think about objects
- * think about template render
- * later
- * ---
- * also think about controls on tasks
- * ---
- * make a slide effect or drag and drop
- * make a notification and animation that user can slide
- * his notes
- * ---
- * save all notes in localstroge and render they
- */
 const notes = [
 	{
 		task: '123',
@@ -34,13 +17,6 @@ const notes = [
 		status: true,
 	},
 ];
-
-/**
- * think about make one more object in object
- * make variable newNote into notes to try to save data
- * from input value and then push it into template
- *
- */
 
 input.addEventListener('keydown', () => {
 	//fix the bag with empty input later
@@ -79,10 +55,7 @@ addBtn.addEventListener('click', () => {
 });
 
 //template fucntion for tasks
-/**
- * check the status from the class
- * if status true classname will change to 'complete'
- */
+
 function noteTemplate(note) {
 	taskList.insertAdjacentHTML(
 		'beforeend',
